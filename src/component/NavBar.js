1736ab8032976eabcbe0e8114a34476114eec0ab
Anthony <alasam@live.com>
2022-10-13 03:30:49 -0400

    add specifics to navbar, add future states for links to be made

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,30 +1,36 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+
+import { useState } from "react";
+
 
 function Navbar() {
+  const [activeLink, setActiveLink] = useState("home);")
+
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        // Brand/Name
+        <Navbar.Brand href="#home">Anthony Lasam</Navbar.Brand>
+        // Mobile Navbar minimized
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <span className="navbar-toggler-icon"></span>
+        </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="#info">Info</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
           </Nav>
+          <span className="navbar-text">
+            <div className="social-icon">
+              <a href="#"><img src={} alt="" /></a>
+              <a href="#"><img src={} alt="" /></a>
+              <a href="#"><img src={} alt="" /></a>
+            </div>
+            <button className="vvd" onClick={() => console.log('connect')}><span>Let's Connect</span></button>
+          </span>
         </Navbar.Collapse>
       </Container>
     </Navbar>
