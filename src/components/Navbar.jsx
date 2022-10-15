523660819d2cb827b77c295656474201b939a163
Anthony <alasam@live.com>
2022-10-15 18:52:04 -0400

    add css to more items, setup nav state

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaBars, Fatimes } from "react-icons/fa";
 
 const Navbar = () => {
+  const [Nav, setNav] = useState(false);
+  const handleClick = () => setNav(!Nav);
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0A192f] text-gray-300">
       {/* logo/name */}
@@ -17,16 +19,16 @@ const Navbar = () => {
       </ul>
 
       {/* menu icon */}
-      <div className="hidden">
+      <div onClick={handleClick} className="md:hidden z-10">
         <FaBars />
       </div>
 
       {/* mobile menu */}
-      <ul className="hidden">
-        <li>Home</li>
-        <li>About</li>
-        <li>Work</li>
-        <li>Contact</li>
+      <ul className="hidden absolute top-0 left-0 w-full h-screen bg-[#0A192f] flex flex-col justify-center items-center">
+        <li className="py-6 text-4xl">Home</li>
+        <li className="py-6 text-4xl">About</li>
+        <li className="py-6 text-4xl">Work</li>
+        <li className="py-6 text-4xl">Contact</li>
       </ul>
     </div>
   );
