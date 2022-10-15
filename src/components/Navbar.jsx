d02600ce34ea16b8f0565e62efcd37633625cd05
Anthony <alasam@live.com>
2022-10-15 02:49:05 -0400

    add inital navbar items, add name and list of links, add momobile elements for later

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { FaBars, Fatimes } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0A192f] text-gray-300">
+      {/* logo/name */}
+      <div>Anthony Lasam</div>
+
+      {/* menu */}
+      <div>
+        <ul className="flex">
+          <li>Home</li>
+          <li>About</li>
+          <li>Work</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+
+      {/* menu icon */}
+      <div className="hidden">
+        <FaBars />
+      </div>
+
+      {/* mobile menu */}
+      <ul className="hidden">
+        <li>Home</li>
+        <li>About</li>
+        <li>Work</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
