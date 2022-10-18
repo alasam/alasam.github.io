4df3cf9fc1f6a5f722f78ef8afb6e47036f991d2
Anthony <alasam@live.com>
2022-10-18 01:56:10 -0400

    create grid and add logos and titles for skills

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import About from "./components/About";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import Skills from "./components/Skills";
 import Socials from "./components/Socials";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Socials />
       <Home />
       <About />
+      <Skills />
     </div>
   );
 }
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,7 +5,7 @@ import GitHub from "../assets/github.png";
 import HTML from "../assets/html.png";
 import JavaScript from "../assets/javascript.png";
 import Node from "../assets/node.png";
-import Postgresql from "../assets/post.png";
+import Postgresql from "../assets/postgresql.png";
 import Rails from "../assets/rails.png";
 import ReactLogo from "../assets/react.png";
 import Ruby from "../assets/ruby.png";
@@ -13,16 +13,58 @@ import Tailwind from "../assets/tailwind.png";
 
 const Skills = () => {
   return (
-    <div name="skills">
+    <div name="skills" className="w-full h-screen bg-black text-gray-300">
       {/* Container */}
-      <div>
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div>
-          <p>Experience</p>
-          <p>These are the technologies I work with</p>
+          <p className="text-4xl font-bold inline border-b-4 border-blue-500">
+            Skills
+          </p>
+          <p className="py-4">These are the technologies I work with</p>
         </div>
-        <div>
-          <img src={HTML} alt="HTML logo" />
-          <p>HTML</p>
+
+        {/* icon container */}
+        <div className="w-full grid grid-cols-2 sm:grid-cols-5 gap-4 text-center py-8">
+          <div className="shadow-md shadow-gray-700 hover:scale-110 duration-500">
+            <img src={HTML} alt="HTML logo" className="w-20 mx-auto" />
+            <p className="py-4">HTML</p>
+          </div>
+          <div className="shadow-md shadow-gray-700 hover:scale-110 duration-500">
+            <img src={CSS} alt="HTML logo" className="w-20 mx-auto" />
+            <p className="py-4">CSS</p>
+          </div>
+          <div className="shadow-md shadow-gray-700 hover:scale-110 duration-500">
+            <img src={GitHub} alt="HTML logo" className="w-20 mx-auto" />
+            <p className="py-4">Git/GitHub</p>
+          </div>
+          <div className="shadow-md shadow-gray-700 hover:scale-110 duration-500">
+            <img src={JavaScript} alt="HTML logo" className="w-20 mx-auto" />
+            <p className="py-4">JavaScript</p>
+          </div>
+          <div className="shadow-md shadow-gray-700 hover:scale-110 duration-500">
+            <img src={Node} alt="HTML logo" className="w-20 mx-auto" />
+            <p className="py-4">Node JS</p>
+          </div>
+          <div className="shadow-md shadow-gray-700 hover:scale-110 duration-500">
+            <img src={ReactLogo} alt="HTML logo" className="w-20 mx-auto" />
+            <p className="py-4">React</p>
+          </div>
+          <div className="shadow-md shadow-gray-700 hover:scale-110 duration-500">
+            <img src={Tailwind} alt="HTML logo" className="w-20 mx-auto" />
+            <p className="py-4">Tailwind CSS</p>
+          </div>
+          <div className="shadow-md shadow-gray-700 hover:scale-110 duration-500">
+            <img src={Postgresql} alt="HTML logo" className="w-20 mx-auto" />
+            <p className="py-4">PostgreSQL</p>
+          </div>
+          <div className="shadow-md shadow-gray-700 hover:scale-110 duration-500">
+            <img src={Ruby} alt="HTML logo" className="w-20 mx-auto" />
+            <p className="py-4">Ruby</p>
+          </div>
+          <div className="shadow-md shadow-gray-700 hover:scale-110 duration-500">
+            <img src={Rails} alt="HTML logo" className="w-20 mx-auto" />
+            <p className="py-4">Ruby on Rails</p>
+          </div>
         </div>
       </div>
     </div>
