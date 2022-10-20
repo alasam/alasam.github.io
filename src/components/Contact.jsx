3c46dba1f2689a0bb0857a8e12dd2d48678ce91f
Anthony <alasam@live.com>
2022-10-20 03:10:33 -0400

    finish front-end of contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,10 +6,35 @@ const Contact = () => {
       name="contact"
       className="w-full h-screen bg-black flex justify-center items-center p-4"
     >
-      <form action="" className="flex flex-col max-w-[600px] w-full">
-        <p className="text-4xl font-bold inline border-b-4 border-blue-500 text-white">
-          Contact
-        </p>
+      {/* form container */}
+      <form action="" className="flex flex-col max-w-[600px] w-full text-white">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-blue-500">
+            Contact
+          </p>
+          <p className="py-4">These are the technologies I work with</p>
+        </div>
+        <input
+          type="text"
+          placeholder="Name"
+          name="email"
+          className="my-4 p-2 bg-white text-black"
+        />
+        <input
+          type="Email"
+          placeholder="Email"
+          name="email"
+          className=" my-4 p-2 bg-white text-black"
+        />
+        <textarea
+          name="message"
+          rows="10"
+          placeholder="Message"
+          className="bg-white p-2 my-4"
+        />
+        <button className="text-white border-2 px-4 py-3 my-8 mx-auto flex items-center hover:bg-green-800 hover:border-green-800">
+          Connect Now!
+        </button>
       </form>
     </div>
   );
