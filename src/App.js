7d2e9e861bc1263f3b50c88097a35697770f75cd
Anthony <alasam@live.com>
2022-10-20 01:02:06 -0400

    create Contact.jsx, add some content and css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import About from "./components/About";
+import Contact from "./components/Contact";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
@@ -14,6 +15,7 @@ function App() {
       <About />
       <Skills />
       <Projects />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/Contact.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div
+      name="contact"
+      className="w-full h-screen bg-black flex justify-center items-center p-4"
+    >
+      <form action="" className="flex flex-col max-w-[600px] w-full">
+        <p className="text-4xl font-bold inline border-b-4 border-blue-500 text-white">
+          Contact
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
