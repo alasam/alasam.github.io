8c03b4dd6bbac304ac76570a3266c2bfec514a1d
Anthony <alasam@live.com>
2022-10-20 03:14:59 -0400

    add endpoints for contact form using getform.io

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,7 +7,11 @@ const Contact = () => {
       className="w-full h-screen bg-black flex justify-center items-center p-4"
     >
       {/* form container */}
-      <form action="" className="flex flex-col max-w-[600px] w-full text-white">
+      <form
+        method="POST"
+        action="https://getform.io/f/69fcb290-3b61-483e-af1b-1d44ad8c19c4"
+        className="flex flex-col max-w-[600px] w-full text-white"
+      >
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-blue-500">
             Contact
@@ -17,7 +21,7 @@ const Contact = () => {
         <input
           type="text"
           placeholder="Name"
-          name="email"
+          name="name"
           className="my-4 p-2 bg-white text-black"
         />
         <input
@@ -30,7 +34,7 @@ const Contact = () => {
           name="message"
           rows="10"
           placeholder="Message"
-          className="bg-white p-2 my-4"
+          className="bg-white p-2 my-4 text-black"
         />
         <button className="text-white border-2 px-4 py-3 my-8 mx-auto flex items-center hover:bg-green-800 hover:border-green-800">
           Connect Now!
